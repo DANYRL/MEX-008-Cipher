22feba037fa8b60ed6d0cb9b0b1eaa66180d1f16
DanyRL <darel_44@yahoo.com.mx>
2019-06-14 15:35:50 -0500

    Unificando archivo html

---
+++ src/index.js
@@ -0,0 +1,8 @@
+alert ('Introduce tu nombre');
+//Declarando la función para imprimir el nombre
+function printName () {
+//Obtener nombre para guardarlo en variable
+let userName= document.getElementById('user-name').value;
+console.log(userName);
+
+}
