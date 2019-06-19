d8ac1c5b95d372ee3105f0459c8f75985fe91c21
DanyRL <darel_44@yahoo.com.mx>
2019-06-19 02:47:11 -0500

    Ligando cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,29 @@
-window.cipher = {//
+window.cipher ={//Cipher es objeto se tiene que crear dos metodos de cifrar y descifrar dentro de este objeto, los metodos son funciones que estan dentro de un objeto
+  encode:(offset, string)=>{//Metodo encode(offset,string)
+    let ascciEncrypted="";//Declara la variable ascciEncrypted donde se guarda el caracter cifrado
+    for (let i=0;i<string.length;i++){//realiza un ciclo for desde 0 hasta tamano-1
+      let ascci=string.charCodeAt(i);
+      let ciffer;
+      if(97<=ascci<=122)
+      {
+        ciffer=(string.charCodeAt(i)-97+parseInt(offset))%26+97;//formula para el cifrado cesar
+      }
 
+      else if(65<=ascci<=90)
+      {
+        ciffer=(string.charCodeAt(i)-65+parseInt(offset))%26+65;//formula para el cifrado cesar
+      }
+
+      ascciEncrypted=ascciEncrypted+String.fromCharCode(ciffer);
+    }
+    return ascciEncrypted;
+  },
+   decode:(offset, string)=>{//Metodo decode(offset,string)
+   let ascciiDecrypted="";//Declara la variable ascciDecrypted donde se guarda el caracter descifrado
+   for (let i=0;i<string.length;i++){//realiza un ciclo  for desde o hasta tamano-1
+     let ciffer=90-(90-string.charCodeAt(i)+parseInt(offset))%26;//formula para el descifrado cesar
+     ascciiDecrypted=ascciiDecrypted+String.fromCharCode(ciffer);
+     }
+    return ascciiDecrypted;
+    },
 };
--- src/index.js
+++ src/index.js
@@ -1,45 +1,45 @@
 //Declaracion de variables
 const inicio=document.getElementById("inicio");
-//const ingresarReferencia=document.getElementById("ingresar-referencia");
+const nivelDeSeguridadElegido=document.getElementById("nivel-de-seguridad-elegido");
+const nivelDeSeguridadOtorgado=document.getElementById("nivel-de-seguridad-otorgado");
+const ingresarReferencia=document.getElementById("ingresar-referencia");
 const iniciarCifrado=document.getElementById("iniciar-cifrado");
-//const ingresarCodigo=document.getElementById("ingresar-codigo");
+const ingresarCodigo=document.getElementById("ingresar-codigo");
 const iniciarDescifrado=document.getElementById("iniciar-descifrado");
 const paginaDeCifrado=document.getElementById("pagina-de-cifrado");
-//const mostrarCodigo=document.getElementById("mostrar-codigo");
+const mostrarCodigo=document.getElementById("mostrar-codigo");
 const paginaDeDescifrado=document.getElementById("pagina-de-descifrado");
-//const mostrarReferencia=document.getElementById("mostrar-referencia");
+const mostrarReferencia=document.getElementById("mostrar-referencia");
 
 //Declaracion de funciones
-const cifrarReferencia = () => {//constante pagina de CIFRADO
+const irAPaginaDeCifrado = () => {//constante pagina de CIFRADO
   paginaDeCifrado.classList.remove('disappear');//aparece página donde se va a mostrar la referencia ENCRIPTADA
   inicio.classList.add('disappear');//desaparece página donde indica qué quieres hacer
 }
 
-const descifrarReferencia=()=>{//constante página de DESCIFRADO
+const irAPaginaDeDescifrado=()=>{//constante página de DESCIFRADO
   paginaDeDescifrado.classList.remove('disappear');//aparece página donde se va a revelar la referencia (DESCIFRADO)
   inicio.classList.add('disappear');//desaparece página donde indica qué quieres hacer
 }
 
-// const encryptionReference=()=>{//Crear constante para mostrar REFERENCIA CIFRADA
-//   showEncryptedPage.classList.remove('disappear');
-//   root.classList.add('disappear');
-//   let string=document.getElementById("message-to-hide").value;/*Obtiene el valor de la variable string*/
-//   let offset=document.getElementById("hide-key").value;/*Obtiene el valor de la variable offset*/
-//   document.getElementById("hide-message").innerHTML = window.cipher.encode(offset,string);
-// }
-//
-// const showDiscoverMessage=()=>{
-//   showDiscoverMessagePage.classList.remove('disappear');
-//   discoverPage.classList.add('disappear');
-//   const string=document.getElementById("message-to-discover").value;/*Obtiene el valor de la variable string*/
-//   const offset=document.getElementById("discover-key").value;/*Obtiene el valor de la variable offset*/
-//   document.getElementById("discover-message").innerHTML = window.cipher.decode(offset,string);
-// }
+const mostrarCodigoSecreto=()=>{//Crear constante para mostrar REFERENCIA CIFRADA
+   mostrarCodigo.classList.remove('disappear');
+   inicio.classList.add('disappear');
+   let string=document.getElementById("ingresar-referencia").value;/*Obtiene el valor de la variable string*/
+   let offset=document.getElementById("nivel-de-seguridad-elegido").value;/*Obtiene el valor de la variable offset*/
+   document.getElementById("mostrar-codigo").innerHTML = window.cipher.encode(offset,string);
+}
 
-/*Declaracion de eventos*/
+const mostrarReferenciaCifrada=()=>{
+   mostrarReferencia.classList.remove('disappear');
+   inicio.classList.add('disappear');
+   const string=document.getElementById("ingresar-codigo").value;/*Obtiene el valor de la variable string*/
+   const offset=document.getElementById("nivel-de-seguridad-otorgado").value;/*Obtiene el valor de la variable offset*/
+   document.getElementById("mostrar-referencia").innerHTML = window.cipher.decode(offset,string);
+}
 
-iniciarCifrado.addEventListener('click',cifrarReferencia);
-iniciarDescifrado.addEventListener('click',descifrarReferencia);
-// discover.addEventListener('click',showDiscoverPage);
-// hideButton.addEventListener('click',showHideMessage);
-// discoverButton.addEventListener('click',showDiscoverMessage);
+//Declarando eventos
+iniciarCifrado.addEventListener('click',irAPaginaDeCifrado);
+iniciarDescifrado.addEventListener('click',irAPaginaDeDescifrado);
+iniciarCifrado.addEventListener('click',mostrarCodigoSecreto);
+iniciarDescifrado.addEventListener('click',mostrarReferenciaCifrada);
